53de03f3511ef97c4f9075ebe0328d8072943d88
wonder-H <soimar@naver.com>
2022-09-14 21:58:03 +0900

    feat: Calender컴포넌트 생성/달력 적용
    - Calender 컴포넌트 생성
    - pages/reservation에 있는 index.js에 적용
    - react-datepicker로 달력 기능 사용
    
    chore: react-datepicker 라이브러리 설치

---
+++ src/components/Calender.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import DatePicker from 'react-datepicker'
+import { ko } from 'date-fns/esm/locale'
+
+import 'react-datepicker/dist/react-datepicker.css'
+
+const Calender = () => {
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(null)
+  const onChange = (dates) => {
+    const [start, end] = dates
+    setStartDate(start)
+    setEndDate(end)
+  }
+
+  return (
+    <>
+      <DatePicker
+        locale={ko}
+        selected={startDate}
+        onChange={onChange}
+        startDate={startDate}
+        endDate={endDate}
+        selectsRange
+        inline
+      />
+    </>
+  )
+}
+export default Calender
---
+++ src/views/pages/reservation/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Calender from 'src/components/Calender'
+
+function index() {
+  return (
+    <>
+      <Calender />
+    </>
+  )
+}
+
+export default index
