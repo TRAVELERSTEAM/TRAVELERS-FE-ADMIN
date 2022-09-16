85845e7640d1232c57c2184d96b305f92f2947e6
wonder-H <soimar@naver.com>
2022-09-17 00:31:20 +0900

    style: 왼쪽nav바 메뉴들 이름 변경(가독성 고려)/#9

--- src/components/CoreUi/_nav.js
+++ src/components/CoreUi/_nav.js
@@ -3,25 +3,25 @@ import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
 const _nav = [
   {
     component: CNavTitle,
-    name: '메뉴 타이틀',
+    name: '메뉴 타이틀 영역',
   },
   {
     component: CNavItem,
-    name: 'UI예시보기(메뉴1)',
+    name: '⭐UI예시보기(메뉴 영역)',
     to: '/ui',
   },
   {
     component: CNavGroup,
-    name: '그룹메뉴 타이틀',
+    name: '그룹메뉴 타이틀 영역',
 
     items: [
       {
         component: CNavItem,
-        name: '메뉴a',
+        name: '메뉴 영역1',
       },
       {
         component: CNavItem,
-        name: '메뉴b',
+        name: '메뉴 영역2',
       },
     ],
   },
