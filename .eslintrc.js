7c50f371a9d7f93b6405496c716816708fac1abe
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-09-16 13:56:15 +0900

    conf: eslint 설정에 prettier추가/#6

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,10 +17,12 @@ module.exports = {
     'react-app/jest',
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'prettier',
   ],
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', 'prettier'],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
 }
