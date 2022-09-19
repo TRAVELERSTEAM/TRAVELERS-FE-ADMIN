d1037135e7f358b7368b03808ea354467bff1a06
Mangrove-Sun <tjsghd0317@gmail.com>
2022-09-19 20:45:38 +0900

    feat: 텍스트에디터 ref넘겨받기 해결 및 툴바기능 정리

--- src/components/texteditor/TextEditor.js
+++ src/components/texteditor/TextEditor.js
@@ -1,34 +1,24 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 
-const TextEditor = (quillRef, htmlContent, setHtmlContent) => {
-  console.log(quillRef)
-  const modules = useMemo(
-    () => ({
-      toolbar: {
-        // 툴바에 넣을 기능들을 순서대로 나열하면 된다.
-        container: [
-          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-          [{ size: ['small', false, 'large', 'huge'] }, { color: [] }, { background: [] }],
-          [{ list: 'ordered' }, { list: 'bullet' }, { align: [] }],
-          ['link', 'image', 'video'],
-          ['clean'],
-        ],
-        // handlers: { // 위에서 만든 이미지 핸들러 사용하도록 설정
-        //     image: imageHandler,
-        // },
-      },
-    }),
-    [],
-  )
-
+const TextEditor = (prop, ref) => {
+  const { value, setValue, placeHolder } = prop
+  const modules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'], // toggled buttons, block
+      [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+      [{ color: [] }, { background: [] }], // dropdown with defaults
+      [{ list: 'ordered' }, { list: 'bullet' }], // lists
+      ['link', 'image'],
+      ['clean'],
+    ],
+  }
   const formats = [
     'header',
     'font',
     'background',
     'color',
-    'code',
     'size',
     'bold',
     'italic',
@@ -43,31 +33,30 @@ const TextEditor = (quillRef, htmlContent, setHtmlContent) => {
     'direction',
     'link',
     'image',
-    'code-block',
     'formula',
-    'video',
   ]
-  const styles = {
-    height: '400px',
-  }
-  const textHandler = () => {
-    console.log(quillRef.current)
+  const textHandler = (e) => {
+    setValue(e)
+    // console.log(value)
+    // console.log('e', e)
   }
 
   return (
     <div>
       <ReactQuill
-        // ref={quillRef}
-        style={styles}
+        ref={ref}
         theme="snow"
         modules={modules}
         formats={formats}
-        value={htmlContent}
-        onChange={setHtmlContent}
-        placeholder="상품 상세 페이지를 작성하세요!"
+        value={value}
+        onChange={textHandler}
+        placeholder={placeHolder}
       ></ReactQuill>
     </div>
   )
 }
 
-export default TextEditor
+// 함수 컴포넌트에는 인스턴스가 없어 ref 어트리뷰트를 사용할 수 없다.
+// 때문에 forwardRef를 사용
+const forwardedTextEditor = React.forwardRef(TextEditor)
+export default forwardedTextEditor
