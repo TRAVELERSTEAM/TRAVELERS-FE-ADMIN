4c8627789a662fd2386c7cbd63c1e0a3c42ee92d
Mangrove-Sun <tjsghd0317@gmail.com>
2022-10-08 18:12:56 +0900

    feat: Reference Api get 연결/#30

--- src/pages/Reference.tsx
+++ src/pages/Reference.tsx
@@ -1,7 +1,15 @@
+import { getReferenceAll } from '@/api/reference/Reference';
 import React from 'react';
+import { useQuery } from 'react-query';
 
 function Reference() {
-  return <div>Reference</div>;
+  const { data } = useQuery('reference', getReferenceAll);
+  console.log(data);
+  return (
+    <>
+      <h2>자료실</h2>
+    </>
+  );
 }
 
 export default Reference;
