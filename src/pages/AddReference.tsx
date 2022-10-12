50cc9e128c0c4b7c86672815e6261af99acebda3
Mangrove-Sun <tjsghd0317@gmail.com>
2022-10-13 03:14:40 +0900

    feat: 자료실 글 등록 폼 구현/#30

--- src/pages/AddReference.tsx
+++ src/pages/AddReference.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import AddNotifyForm from '@/components/AddNotifyForm';
+import { useMutation, useQueryClient } from 'react-query';
+import { addReference, PostRequestProps } from '@/api/reference/Reference';
+
+interface InputRef {
+  value: string;
+}
 
 function AddReference() {
-  return <div>AddReference</div>;
+  const inputRef = useRef<InputRef[]>([]);
+  const reqData = { title: '', content: '' };
+  // const [reqData, setReqData] = useState({ title: '', content: '' });
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation('addReference', (data: PostRequestProps) => addReference(data));
+
+  const addHandler = () => {
+    reqData.title = inputRef.current[0].value;
+    reqData.content = inputRef.current[1].value;
+    mutate(reqData, {
+      onSuccess: () => {
+        queryClient.invalidateQueries('reference');
+      },
+    });
+    console.log(reqData);
+  };
+
+  return (
+    <>
+      <div>
+        <AddNotifyForm ref={inputRef} place={'자료실'} title={''} content={''} />
+      </div>
+      <button onClick={addHandler}>등록</button>
+    </>
+  );
 }
 
 export default AddReference;
