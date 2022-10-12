b811bf09023f44d16ecbf72ed899e8a8856cac45
Mangrove-Sun <tjsghd0317@gmail.com>
2022-10-13 03:25:37 +0900

    feat: useQuery refetchOnWindowFocus 옵션추가/#30

--- src/pages/Reference.tsx
+++ src/pages/Reference.tsx
@@ -6,21 +6,23 @@ import { Contents } from '@/api/reference/Reference';
 
 function Reference() {
   const [posts, setPosts] = useState<Contents[]>();
-  const { data, isLoading, error } = useQuery('reference', getReferenceAll);
-  const navigator = useNavigate();
 
-  console.log(data);
+  const { data, isLoading, error } = useQuery('reference', getReferenceAll, {
+    refetchOnWindowFocus: false,
+  });
+
+  const navigator = useNavigate();
 
   useEffect(() => {
     if (data?.contents.length !== 0) setPosts(data?.contents);
-  }, [isLoading]);
+  }, [data]);
 
   if (isLoading) <p>Loading...</p>;
   if (error) <p>Error!!!</p>;
   return (
     <>
       <h2>자료실</h2>
-      <button onClick={() => navigator('/')} className="add-reference">
+      <button onClick={() => navigator('/dashboard/add-reference')} className="add-reference">
         + New
       </button>
       <table>
@@ -37,7 +39,7 @@ function Reference() {
             {posts.map((post, index) => {
               return (
                 <tr key={index}>
-                  <td>{post.id}</td>
+                  <td>{post.sequence}</td>
                   <td>{post.title}</td>
                   <td>{post.writer}</td>
                   <td>{post.createdAt}</td>
