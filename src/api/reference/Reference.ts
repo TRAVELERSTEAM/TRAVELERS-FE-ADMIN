94b757b1ecf4c605499c1e98bb3fd24dd8fbda89
Mangrove-Sun <tjsghd0317@gmail.com>
2022-10-13 03:15:18 +0900

    feat: Reference API POST 연결/#30

--- src/api/reference/Reference.ts
+++ src/api/reference/Reference.ts
@@ -5,7 +5,7 @@ const { VITE_APP_ADMIN_APIKEY, VITE_APP_BASE_URL } = import.meta.env;
 const accessToken = VITE_APP_ADMIN_APIKEY;
 const baseUrl = VITE_APP_BASE_URL;
 
-interface RefReturnProps {
+interface GetReturnProps {
   contents: Contents[];
   currentPage: number;
   endPage: number;
@@ -23,12 +23,38 @@ export interface Contents {
   writer: string;
 }
 
-// 자료실 데이터 모두 가져오기
-export const getReferenceAll = async (): Promise<RefReturnProps> => {
-  const { data } = await axios.get(`${baseUrl}/notify/ref-library`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  return data;
+export interface PostRequestProps {
+  content: string;
+  title: string;
+}
+
+// GET 자료실 데이터 모두 가져오기
+export const getReferenceAll = async (): Promise<GetReturnProps | undefined> => {
+  try {
+    const { data } = await axios.get(`${baseUrl}/notify/ref-library`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
+
+// POST 자료실 글 등록
+export const addReference = async (payload: PostRequestProps): Promise<void> => {
+  try {
+    const { status } = await axios.post(`${baseUrl}/notify/ref_library`, payload, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    if (status === 201) {
+      console.log(true);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
