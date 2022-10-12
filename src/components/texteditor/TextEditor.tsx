cbf050771424398f39aa15d5699168795fdc31d9
Mangrove-Sun <tjsghd0317@gmail.com>
2022-10-12 16:37:27 +0900

    feat: Image 업로드 API 연결해 url값 받기/#35

--- src/components/texteditor/TextEditor.tsx
+++ src/components/texteditor/TextEditor.tsx
@@ -1,19 +1,90 @@
-import React from 'react';
+import axios, { AxiosError } from 'axios';
+import React, { useMemo } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
-const TextEditor = (prop, ref) => {
-  const { value, setValue, placeHolder } = prop;
-  const modules = {
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'], // toggled buttons, block
-      [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
-      [{ color: [] }, { background: [] }], // dropdown with defaults
-      [{ list: 'ordered' }, { list: 'bullet' }], // lists
-      ['link', 'image'],
-      ['clean'],
-    ],
+const { VITE_APP_ADMIN_APIKEY, VITE_APP_IMAGE_URL } = import.meta.env;
+
+const accessToken = VITE_APP_ADMIN_APIKEY;
+const baseUrl = VITE_APP_IMAGE_URL;
+
+interface DataProps {
+  textData: string | undefined;
+  setTextData: React.Dispatch<React.SetStateAction<undefined>>;
+  placeHolder: string;
+}
+const TextEditor = (prop: DataProps, ref: any) => {
+  const { textData, setTextData, placeHolder } = prop;
+
+  const imageHandler = () => {
+    // 파일을 업로드 하기 위한 input 태그 생성
+    const input = document.createElement('input');
+    const formData = new FormData();
+    let url = '';
+
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+
+    // 파일이 input 태그에 담기면 실행 될 함수
+    input.onchange = async () => {
+      const file = input.files;
+      if (file !== null) {
+        formData.append('files', file[0]);
+
+        try {
+          const { data } = await axios.post(`${baseUrl}/image`, formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+
+          url = data[0];
+
+          // 커서의 위치를 알고 해당 위치에 이미지 태그를 넣어주는 코드
+          // 해당 DOM의 데이터가 필요하기에 props로 받은 ref를 사용한다.
+          const range = ref.current?.getEditor().getSelection()?.index;
+          if (range !== null && range !== undefined) {
+            const quill = ref.current?.getEditor();
+
+            quill?.setSelection(range, 1);
+
+            quill?.clipboard.dangerouslyPasteHTML(range, `<img src=${url} alt="이미지" />`);
+          }
+
+          return { ...data, success: true };
+        } catch (error) {
+          const err = error as AxiosError;
+          return { ...err.response, success: false };
+        }
+      }
+    };
   };
+
+  // 텍스트를 입력할 때마다 imageHandler 때문에 focus가 계속 풀리게 되어 useMemo 사용.
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{ size: ['small', false, 'large', 'huge'] }, { color: [] }, { background: [] }],
+          [
+            { list: 'ordered' },
+            { list: 'bullet' },
+            { indent: '-1' },
+            { indent: '+1' },
+            { align: [] },
+          ],
+          ['link', 'image'],
+        ],
+        handlers: {
+          image: imageHandler,
+        },
+      },
+    }),
+    [],
+  );
   const formats = [
     'header',
     'font',
@@ -35,20 +106,22 @@ const TextEditor = (prop, ref) => {
     'image',
     'formula',
   ];
-  const textHandler = (e) => {
-    setValue(e);
-    // console.log(value)
-    // console.log('e', e)
+  const textHandler = (e: any) => {
+    setTextData(e);
   };
 
   return (
     <div>
       <ReactQuill
-        ref={ref}
+        ref={(element) => {
+          if (element !== null) {
+            ref.current = element;
+          }
+        }}
         theme="snow"
         modules={modules}
         formats={formats}
-        value={value}
+        value={textData}
         onChange={textHandler}
         placeholder={placeHolder}
       ></ReactQuill>
