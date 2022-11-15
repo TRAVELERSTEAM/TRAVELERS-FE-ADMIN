81ef6f5215bdbbd6a0ed47369426155c49114081
Mangrove-Sun <tjsghd0317@gmail.com>
2022-11-15 22:43:01 +0900

    feat: 카테고리 선택 시 상품 필터링하여 출력/#42

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,17 +1,15 @@
 import { getAllProduct, ProductProps } from '@/api/product/product';
 import ProductCardLayout from '@/layouts/ProductCardLayout';
 import { styled } from '@material-ui/core/styles';
-import { Grid, useMediaQuery } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 
 function ProductList() {
+  const [filtered, setFilterd] = useState<string>('all');
+  const [products, setProducts] = useState<ProductProps[]>();
   const { data, isLoading, isError } = useQuery('getAllProduct', getAllProduct, {
     refetchOnWindowFocus: false,
   });
-  console.log(data);
-  const matches = useMediaQuery('(min-width: 540px');
-  console.log(matches);
 
   function productCard(products: ProductProps[] | undefined) {
     return products?.map((product) => {
@@ -28,18 +26,66 @@ function ProductList() {
     });
   }
 
+  useEffect(() => {
+    if (filtered === 'all') return setProducts(data);
+    setProducts(data?.filter((el: ProductProps) => el.target === filtered));
+  }, [data, filtered]);
+
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error!!!</p>;
-  return <Container>{productCard(data)}</Container>;
+  return (
+    <Container>
+      <FilterArea>
+        <select
+          className="product-filter"
+          name="product-filter"
+          id="filter"
+          defaultValue={'all'}
+          onChange={(e) => setFilterd(e.target.value)}
+        >
+          <option value="all">== 카테고리 ==</option>
+          <option value="5070끼리">5070끼리</option>
+          <option value="3040끼리">3040끼리</option>
+          <option value="2040끼리">2040끼리</option>
+          <option value="남자끼리">남자끼리</option>
+          <option value="여자끼리">여자끼리</option>
+          <option value="누구든지">누구든지</option>
+          <option value="자녀동반">자녀동반</option>
+        </select>
+        <span className="filterd-count">{`상품 ${products?.length}개`}</span>
+      </FilterArea>
+      <Contant>{productCard(products)}</Contant>
+    </Container>
+  );
 }
 
 export default ProductList;
 
-const Container = styled('div')(({ theme }) => ({
+const Container = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+const FilterArea = styled('div')({
+  padding: '20px 0',
+  '.product-filter': {
+    marginRight: '8px',
+    padding: '3px',
+    width: '8rem',
+    borderColor: '#637381',
+    borderRadius: '3px',
+    color: '#637381',
+  },
+  '.filterd-count': {
+    fontSize: '0.9rem',
+  },
+});
+
+const Contant = styled('div')(({ theme }) => ({
   margin: '0 auto',
-  maxWidth: '1300px',
+  width: '100%',
   display: 'grid',
-  gridTemplateColumns: 'repeat(auto-fit, minmax(280px, 1fr))',
+  gridTemplateColumns: 'repeat(auto-fill, minmax(280px, 1fr))',
   gap: '24px 40px',
   [theme.breakpoints.down('md')]: {
     gridTemplateColumns: 'repeat(2, 1fr)',
