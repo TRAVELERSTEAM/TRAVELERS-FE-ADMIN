b86923ce95683d1c7e939184771f1ce83a77d3bc
Mangrove-Sun <tjsghd0317@gmail.com>
2022-11-27 15:30:44 +0900

    feat: 등록되어있는 배너리스트 데이터를 서버에서 받아와 화면에 출력/#48

--- src/pages/MainBanner.tsx
+++ src/pages/MainBanner.tsx
@@ -1,7 +1,83 @@
-import React from 'react';
+import { getAllBanner, ResBannerProps } from '@/api/banner/banner';
+import { styled } from '@material-ui/core/styles';
+import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 
 function MainBanner() {
-  return <div>MainBanner</div>;
+  const [bannerList, setBannerList] = useState<ResBannerProps[]>();
+  const { data } = useQuery('getAllBanner', getAllBanner, {
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    setBannerList(data);
+  }, [data]);
+
+  return (
+    <TableStyle>
+      <thead>
+        <tr className="header-title">
+          <th>번호</th>
+          <th>배너이미지</th>
+          <th>상품 ID</th>
+          <th>제목</th>
+          <th>해쉬태그</th>
+        </tr>
+      </thead>
+      <tbody>
+        {bannerList?.map((banner, index) => {
+          return (
+            <tr key={banner.productId}>
+              <td>{index + 1}</td>
+              <td>
+                <BannerImg src={banner.image} alt={banner.title} />
+              </td>
+              <td className="product-id">
+                <span>{banner.productId}</span>
+              </td>
+              <td>
+                <span>{banner.title}</span>
+              </td>
+              <td>
+                <span>{banner.hashtag}</span>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </TableStyle>
+  );
 }
 
 export default MainBanner;
+
+const TableStyle = styled('table')(({ theme }) => ({
+  fontSize: '18px',
+  wordBreak: 'keep-all',
+  borderCollapse: 'collapse',
+  th: {
+    padding: '5px 0',
+  },
+  'tr, td': {
+    padding: '0 20px',
+    borderBottom: '1px solid #ececec',
+  },
+  '.header-title': {
+    borderBottom: '1px solid #333',
+  },
+  '.product-id': {
+    textAlign: 'center',
+  },
+  [theme.breakpoints.down('md')]: {
+    fontSize: '14px',
+    'tr, td': {
+      padding: '0 5px',
+    },
+  },
+}));
+
+const BannerImg = styled('img')({
+  padding: '4px 14px',
+  width: '100%',
+  maxWidth: '300px',
+});
